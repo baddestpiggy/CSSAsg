eb1021d19fa4cbc8f02a542a577fc1a5cba2ac76
baddestpiggy <scottcjtang@gmail.com>
2026-01-16 02:50:13 +0800

    added dropdown for facts organised separate facts pages

---
+++ my-app/app/facts/Christ/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>christ</h1>
+    </div>
+    
+    
+  );
+}
+
---
+++ my-app/app/facts/Petra/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>petra</h1>
+    </div>
+    
+    
+  );
+}
