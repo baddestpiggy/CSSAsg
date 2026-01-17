7e0ccf94b176c011b91ecce15939611f1bc5cf59
Kynantan01 <kynan_tan@yahoo.com>
2026-01-17 10:23:09 +0800

    cleaned up unecessary files

--- my-app/app/facts/Wall/page.jsx
+++ my-app/app/facts/Wall/page.jsx
@@ -1,14 +1,51 @@
-"use client";
+import styles from "./wall.module.css"
 
-
-
-
-export default function App() {
-  return (
-    <div>
-      <h1>wall</h1>
-    </div>
+export default function China()
+{
     
-    
-  );
+}
+
+function FactBar()
+{
+    return (
+        <div className={styles.factBar}>
+            <div className={styles.factItem}>
+                <img src="/location.png" alt="Location" className={"styles.icon"} />
+                <div>
+                    <span className={styles.label}>Location:</span>
+                    <p className={styles.value}>China</p>
+                </div>
+            </div>
+
+            <div className={styles.divider}></div>
+
+            <div className={styles.factItem}>
+                <img src="/calender.png" alt="Built" className={styles.icon} />
+                <div>
+                    <span className={styles.label}>Built:</span>
+                    <p className={styles.value}>7th Century BC</p>
+                </div>
+            </div>
+        
+            <div className={styles.divider}></div>
+
+            <div className={styles.factItem}>
+                <img src="/material.png" alt="Materials" className={styles.icon} />
+                <div>
+                    <span className={styles.label}>Materials:</span>
+                    <p className={styles.value}>Stone, Brick, Earth</p>
+                </div>
+            </div>
+
+            <div className={styles.divider}></div>
+
+            <div className={styles.factItem}>
+                <img src="/globe.png" alt="Length" className={styles.icon} />
+                <div>
+                    <span className={styles.label}>Length:</span>
+                    <p className={styles.value}>Over 21,000 km</p>
+                </div>
+            </div>
+        </div>
+    );
 }
--- my-app/app/ui/Navbar.jsx
+++ my-app/app/ui/Navbar.jsx
@@ -11,30 +11,6 @@ export default function Navbar() {
         <nav className={styles.navbar}>
             <Link className={styles.navitem} href="/">Home</Link>
             <Link className={styles.navitem} href="/gallery">Gallery</Link>
-<<<<<<< HEAD
-            <div 
-            className={styles.dropdown}
-            onMouseEnter={() => setIsOpen(true)}
-            onMouseLeave={() => setIsOpen(false)}
-            >
-                <Link className={styles.navitem} href="/facts">
-                Facts {isOpen ? '▴' : '▾'}
-                </Link>
-
-                {isOpen && (
-                    <div className={styles.dropdownMenu}>
-                        <Link href="/facts/China" className={styles.dropdownItem}>Great Wall of China</Link>
-                        <Link href="/facts/Petra" className={styles.dropdownItem}>Petra</Link>
-                        <Link href="/facts/Colosseum" className={styles.dropdownItem}>Colosseum</Link>
-                        <Link href="/facts/Chichén Itzá" className={styles.dropdownItem}>Chichén Itzá</Link>
-                        <Link href="/facts/Machu Picchu" className={styles.dropdownItem}>Machu Picchu</Link>
-                        <Link href="/facts/Taj Mahal" className={styles.dropdownItem}>Taj Mahal</Link>
-                        <Link href="/facts/Christ" className={styles.dropdownItem}>Christ the Redemeer</Link>
-                    </div>
-                )}
-            </div>
-            <Link className={styles.navitem} href="/about">About</Link>
-=======
             
             <div class={styles.dropdown}>
                 <p class={styles.dropbtn}>Facts</p>
@@ -52,7 +28,6 @@ export default function Navbar() {
             <a className={styles.navitem} href="/42.zip" download="website.zip">Download the Mystery File</a>
             
 
->>>>>>> eb1021d19fa4cbc8f02a542a577fc1a5cba2ac76
         </nav>
     );
 }
