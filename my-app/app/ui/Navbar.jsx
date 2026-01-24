a1989713aa4056ac1bf0c26736fab31e1af21992
jareth goh <jarethgohzhekai@gmail.com>
2026-01-24 12:33:12 +0800

    fixed my navbar

--- my-app/app/ui/Navbar.jsx
+++ my-app/app/ui/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import styles from "./Navbar.module.css";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -12,19 +12,25 @@ export default function Navbar() {
             <Link className={styles.navitem} href="/">Home</Link>
             <Link className={styles.navitem} href="/gallery">Gallery</Link>
             
-            
-            <div className={styles.dropdown}>
+            <div 
+                className={styles.dropdown}
+                onMouseEnter={() => setIsOpen(true)}
+                onMouseLeave={() => setIsOpen(false)}
+            >
                 <p className={styles.dropbtn}>Facts</p>
-                <div className={styles.dropdowncontent}>
-                    <Link className={styles.dropdowncontentItem} href="/facts/Christ">Christ the Redeemer</Link>
-                    <Link className={styles.dropdowncontentItem} href="/facts/ChichenItza">Chichen Itza</Link>
-                    <Link className={styles.dropdowncontentItem} href="/facts/Colosseo">Colosseum</Link>
-                    <Link className={styles.dropdowncontentItem} href="/facts/MachuPicchu">Machu Picchu</Link>
-                    <Link className={styles.dropdowncontentItem} href="/facts/Petra">Petra</Link>
-                    <Link className={styles.dropdowncontentItem} href="/facts/TajMahal">Taj Mahal</Link>
-                    <Link className={styles.dropdowncontentItem} href="/facts/Wall">Wall</Link>
-                </div>
+                {isOpen && (
+                    <div className={styles.dropdowncontent}>
+                        <Link className={styles.dropdowncontentItem} href="/facts/Christ">Christ the Redeemer</Link>
+                        <Link className={styles.dropdowncontentItem} href="/facts/ChichenItza">Chichen Itza</Link>
+                        <Link className={styles.dropdowncontentItem} href="/facts/Colosseo">Colosseum</Link>
+                        <Link className={styles.dropdowncontentItem} href="/facts/MachuPicchu">Machu Picchu</Link>
+                        <Link className={styles.dropdowncontentItem} href="/facts/Petra">Petra</Link>
+                        <Link className={styles.dropdowncontentItem} href="/facts/TajMahal">Taj Mahal</Link>
+                        <Link className={styles.dropdowncontentItem} href="/facts/Wall">Wall</Link>
+                    </div>
+                )}
             </div>
+            
             <Link className={styles.navitem} href="/Quiz">Quiz</Link>
             <Link className={styles.navitem} href="/about">About</Link>
             <a className={styles.navitem} href="/42.zip" download="website.zip">Download the Mystery File</a>
